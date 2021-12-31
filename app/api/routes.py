e13af389cb41152424d22e83e3cb75b8b5de4f67
purbokusumo <purbo.kusumo@autobahn-security.com>
2021-12-31 11:14:00 +0700

    add route for api product

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,6 +1,9 @@
 from .resources.status import Status
+from app.api.resources.product import Product, Products
 
 
 def init_routes(api):
     """routes"""
     api.add_resource(Status, '/status')
+    api.add_resource(Products, '/products')
+    api.add_resource(Product, '/product/<int:_id>')
